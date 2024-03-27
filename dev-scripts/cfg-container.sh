716d1cd43404728812811431b45f40b7e0356159
deya <deya.sanchez@gmail.com>
2024-03-27 14:37:37 +0000

    chore:comment with ide version added in

--- dev-scripts/cfg-container.sh
+++ dev-scripts/cfg-container.sh
@@ -4,6 +4,7 @@
 # user names after film/tv animated characters
 CFG_ARG_USR="sneezy"
 # name for the image
+# stm32tchain_1.15.0
 CFG_IMAGE_NAME="stm32_tchain"
 # country language
 CFG_LOCALE="en_GB.UTF-8"
