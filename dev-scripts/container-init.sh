d7dc2b6e531c71fcdc99dc5ae0c74ab8be01d0e0
embedded <56156306+parrotrueper@users.noreply.github.com>
2024-04-25 15:18:50 +0100

    Update container-init.sh
    removed reference to script no longer required that was breaking running the container

--- dev-scripts/container-init.sh
+++ dev-scripts/container-init.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-srcfile="./dev-scripts/source-me.sh"
-if [[ ! -e "${srcfile}" ]]; then 
-    echo "ERROR:"
-    echo "${srcfile} NOT FOUND!"
-    exit 1
-fi
-source $srcfile
 printf "To launch STM32 IDE:\n\n"
 printf "$ stm32cubeide&\n\n"
 
